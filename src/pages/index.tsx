8224b32d2778b12debc1a1b7087fbd344564f185
kitsunecat <scooping.fox@gmail.com>
2022-08-06 16:41:41 +0900

    feat: add tweet fn

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,16 @@
+// Next.js
 import type { NextPage } from 'next'
+
+// Amplify
 import { Amplify, API, graphqlOperation } from 'aws-amplify';
-import { createTweet, updateTweet, deleteTweet } from '../graphql/mutations';
+
+// Amplify GraphQL
+import { createTweet, deleteTweet } from '../graphql/mutations';
 import { listTweets } from '../graphql/queries';
 import { onCreateTweet } from '../graphql/subscriptions';
 
 
-import { useCallback, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import awsconfig from '../aws-exports';
 Amplify.configure(awsconfig);
 
@@ -15,20 +20,28 @@ type Tweet = {
   id: String
   updatedAt: String
 }
+
 const Home: NextPage = () => {
   const [tweets, setTweets] = useState<Tweet[]>([])
-
-  /* create a tweet */
-  // await API.graphql(graphqlOperation(createTweet, { input: tweet }));
-  // /* update a tweet */
-  // await API.graphql(graphqlOperation(updateTweet, { input: { id: tweetId, name: "Updated tweet info" } }));
-  // /* delete a tweet */
-  // await API.graphql(graphqlOperation(deleteTweet, { input: { id: tweetId } }));
+  const [newTweet, setNewTweet] = useState<String>("")
 
   const getTweets = async () => {
-    const tweets = await API.graphql(graphqlOperation(listTweets));
+    const tweets = await API.graphql(graphqlOperation(listTweets))
     // @ts-ignore
     setTweets(tweets.data.listTweets.items)
+  }
+
+  const createTweetHandler = async () => {
+    const result = await API.graphql(graphqlOperation(createTweet, { input: { content: newTweet } }))
+    // @ts-ignore
+    setTweets([...tweets, result.data.createTweet])
+    setNewTweet('')
+  }
+
+  const deleteTweetHandler = async (tweetId: string) => {
+    await API.graphql(graphqlOperation(deleteTweet, { input: { id: tweetId } }))
+    // @ts-ignore
+    setTweets(tweets.filter(t => t.id !== tweetId))
   }
 
   useEffect(() => {
@@ -37,16 +50,21 @@ const Home: NextPage = () => {
 
   return (
     <div>
+      <input type="text" onChange={(e) => setNewTweet(e.target.value)} />
+      <button onClick={() => createTweetHandler()}>submit</button>
+
       {tweets.map((tweet) => (
-        <div key="tweet.id">
+        <div key={tweet.id}>
           <p>{tweet.id}</p>
           <p>{tweet.content}</p>
           <p>{tweet.createdAt}</p>
           <p>{tweet.updatedAt}</p>
+          <button onClick={() => deleteTweetHandler(tweet.id)}>delete</button>
           <hr />
         </div>
-      ))}
-    </div>
+      ))
+      }
+    </div >
   )
 }
 
